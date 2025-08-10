9ff474443545f1378a808477cb30709815b17fee
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-10 20:43:48 +0600

    add

---
+++ max3.js
@@ -0,0 +1,14 @@
+function max(num1, num2, num3){
+    if((num1 > num2) && (num1 > num3)){
+        return num1;
+    }
+    else if((num2 > num1) && (num2 > num3)){
+        return num2;
+    }
+    else{
+        return num3;
+    }
+}
+
+const maximum = max(3, 1, 2);
+console.log(maximum);
